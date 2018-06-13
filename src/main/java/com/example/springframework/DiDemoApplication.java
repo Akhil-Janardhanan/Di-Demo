5cdcdcdb091b9c65e8549bc2f9d1a2f388d143a2
Akhil K J <akhil.janardhanan@litmus7.com>
2018-06-13 16:56:43 +0530

    Created a HelloWorldController and Displayed it in Console.

--- src/main/java/com/example/springframework/DiDemoApplication.java
+++ src/main/java/com/example/springframework/DiDemoApplication.java
@@ -2,11 +2,16 @@ package com.example.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.springframework.controller.HelloWorldController;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(DiDemoApplication.class, args);
+		HelloWorldController helloWorldController = context.getBean(HelloWorldController.class);
+		helloWorldController.print();
 	}
 }
---
+++ src/main/java/com/example/springframework/controller/HelloWorldController.java
@@ -0,0 +1,11 @@
+package com.example.springframework.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloWorldController {
+
+	public void print() {
+		System.out.println("############----HELLO WORLD----###########");
+	}
+}
